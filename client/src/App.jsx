75c74df6fd03ae8153a3617094e03cd0037b924c
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2023-10-13 00:20:12 +0600

    Update

---
+++ client/src/App.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [users, setUser] = useState();
+
+  const UserUrl = "http://localhost:3300/users";
+
+  useEffect(() => {
+    fetch(UserUrl)
+      .then((res) => res.json())
+      .then((data) => setUser(data));
+  }, []);
+
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(user);
+
+    fetch(UserUrl, {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
+  return (
+    <>
+      <h1>User Management</h1>
+      Number of user: {users?.length}
+      {users?.map((user, idx) => {
+        return (
+          <li key={idx}>
+            {user?.name} - {user?.email}
+          </li>
+        );
+      })}
+      <br />
+      <h2>Add User</h2>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" />
+        <br />
+        <input type="email" name="email" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
+    </>
+  );
+}
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,36 @@
 const express = require("express");
+var cors = require("cors");
 const app = express();
+
 const port = process.env.PORT || 3300;
+app.use(cors());
+const users = [
+  {
+    id: 1,
+    name: "John",
+    email: "john@example.com",
+  },
+  {
+    id: 2,
+    name: "doe",
+    email: "doe@example.com",
+  },
+  {
+    id: 3,
+    name: "comp",
+    email: "comp@example.com",
+  },
+];
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+app.post("/users", (req, res) => {
+  console.log("posting user");
+  console.log(req.body);
 });
 
 app.listen(port, () => {
